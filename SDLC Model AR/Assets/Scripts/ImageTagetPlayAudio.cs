7dfcc7c6c3b72c9a1a01d0e5369c59aa51fd7145
Harsh1126 <harshb1508@gmail.com>
2019-03-14 19:17:40 +0530

    Waterfall Model

---
+++ SDLC Model AR/Assets/Scripts/ImageTagetPlayAudio.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ImageTagetPlayAudio : MonoBehaviour, ITrackableEventHandler
+ {
+    static int c = 0;
+    private TrackableBehaviour mTrackableBehaviour;
+    public GameObject[] obj;
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        if (c < 1)
+        {
+            if (newStatus == TrackableBehaviour.Status.DETECTED ||
+                newStatus == TrackableBehaviour.Status.TRACKED ||
+                newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+            {
+                // Play audio when target is found
+                GetComponent<AudioSource>().Play();
+                foreach(GameObject o in obj)
+                {
+                    o.SetActive(true);
+                }
+                c += 1;
+
+            }
+            else
+            {
+                // Stop audio when target is lost
+                //audio.Stop();
+            }
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       
+    }
+}
