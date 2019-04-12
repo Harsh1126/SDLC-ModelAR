a128a94534c14e6a3f36f4c5c7002cec9f75a80d
Harsh1126 <harshb1508@gmail.com>
2019-04-13 02:46:24 +0530

    Animated in all

--- SDLC Model AR/Assets/Scripts/SceneManagement.cs
+++ SDLC Model AR/Assets/Scripts/SceneManagement.cs
@@ -15,4 +15,29 @@ public class SceneManagement : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    public void waterModel()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void incModel()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void protyModel()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void spModel()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void radModel()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void agileModel()
+    {
+        SceneManager.LoadScene(6);
+    }
 }
